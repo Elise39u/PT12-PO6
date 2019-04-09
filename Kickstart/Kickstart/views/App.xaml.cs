8133d7faeeaadd5987114f9d667a34cef159576b
justinLaar322 <justin555@live.nl>
2019-04-09 13:12:34 +0200

    Changed Startpage to loginpage and made a static login page

--- Kickstart/Kickstart/views/App.xaml.cs
+++ Kickstart/Kickstart/views/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Kickstart.views;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Kickstart
@@ -11,7 +12,7 @@ namespace Kickstart
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
